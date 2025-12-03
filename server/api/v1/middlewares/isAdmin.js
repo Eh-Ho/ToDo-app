cef5d62ef314ff2c4eed12096cacb9ec1ce3b4bc
Ehsan <ehosseini8001@gmail.com>
2025-12-03 20:55:59 +0330

    isAdmin middleware done

--- server/api/v1/middlewares/isAdmin.js
+++ server/api/v1/middlewares/isAdmin.js
@@ -1,3 +1,16 @@
+const{StatusCodes, ReasonPhrases} = require('http-status-codes');
+
+
 module.exports = (req, res, next) => {
+    allowedRoles = ['admin'];
+
+    if (!req.user) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Authentication required' });
+    }
+
+    if(!allowedRoles.includes(req.user.role)){
+        res.status(StatusCodes.FORBIDDEN).json({message : ReasonPhrases.FORBIDDEN});
+    }
+    
     next();
 };
