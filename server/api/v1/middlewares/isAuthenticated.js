7c81b28e53cc722f85ca48dda73330830087182c
Ehsan <ehosseini8001@gmail.com>
2025-12-05 18:50:59 +0330

    isAuthenticated completed

--- server/api/v1/middlewares/isAuthenticated.js
+++ server/api/v1/middlewares/isAuthenticated.js
@@ -1,3 +1,29 @@
-module.exports = (req, res, next) => {
-    next();
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const { StatusCodes, ReasonPhrases } = require('http-status-codes');
+
+
+module.exports = async(req, res, next) => {
+    try{
+        const token = req.headers('authorization');
+
+        if(!token){
+            // throw error
+        }
+
+        let decoded;
+        try{
+            decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        }catch(err){
+            // error 401
+        }
+        const user = User.findById(decoded.id);
+        if(!user){
+            // error 401
+        }
+        req.user = {...user, id: user._id.toString()};                          
+        next();
+    }catch(error){
+        //trow error
+    }
 };
