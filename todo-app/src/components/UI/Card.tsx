ea9fb5819f152265ccbf8866e589b7014a97a9c0
Ehsan <ehosseini8001@gmail.com>
2025-12-26 12:09:24 +0330

    card component

---
+++ todo-app/src/components/UI/Card.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+
+interface CardProps {
+  children: ReactNode;
+}
+
+const Card = ({ children }: CardProps) => {
+  return (
+    <div className="bg-surface border border-muted flex justify-center w-full max-w-sm h-fit p-8 rounded-2xl shadow-xl inset-shadow-sm">
+      {children}
+    </div>
+  );
+};
+
+export default Card;
