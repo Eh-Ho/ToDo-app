9696cbce5aa3d00443e9644f5926afc309d827c8
Ehsan <ehosseini8001@gmail.com>
2025-12-26 12:09:56 +0330

    layout for auth pages completed

---
+++ todo-app/src/app/layouts/AuthLayout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+
+const AuthLayout = () => {
+  return (
+    <div className="bg-background text-content min-h-screen w-full flex items-center justify-center">
+      <Outlet></Outlet>
+    </div>
+  );
+};
+export default AuthLayout;
