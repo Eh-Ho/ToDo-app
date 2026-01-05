11f571c59e3ba0c665a1c5a3125661b5b4f69ff7
Ehsan <ehosseini8001@gmail.com>
2026-01-05 14:02:49 +0330

    DTO s and validations done

--- server/api/v1/DTOs/taskDTO.js
+++ server/api/v1/DTOs/taskDTO.js
@@ -1,15 +1,25 @@
 const { requireFields } = require("./helpers");
 
 const createDTO = (reqBody) => {
-  requireFields(reqBody, ["title", "description"]);
-  const { title, description } = reqBody;
-  return { title, description };
+  requireFields(reqBody, ["title"]);
+
+  const { title, description, dueDate, tags, listId } = reqBody;
+  return { title, description, dueDate, tags, listId };
 };
 
 const updateDTO = (reqBody) => {
-  requireFields(reqBody, ["title", "description", "completed"]);
-  const { title, description, completed } = reqBody;
-  return { title, description, completed };
+  requireFields(reqBody, ["title", "completed"]);
+
+  const { title, description, completed, dueDate, tags, listId } = reqBody;
+
+  return {
+    title,
+    description,
+    completed,
+    dueDate,
+    tags,
+    listId,
+  };
 };
 
 module.exports = { createDTO, updateDTO };
